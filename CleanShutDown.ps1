944f23e7b5a140182c3b596c7201028c9a19cc53
Steven Owatemi <isosteven@gmail.com>
2022-09-16 18:07:42 +0100

    clean up

--- CleanShutDown.ps1
+++ CleanShutDown.ps1
@@ -2,18 +2,24 @@ Write-Host "
 Shutdown Script v0.1
 ===============================================
 Hi $env:UserName
-Gonna close all the apps and shutdown $env:ComputerName
+Closing all the apps and shutdown $env:ComputerName
 
 "
+#Just giving the end-user a chance to read the print 
 Start-Sleep 5
+
+#Add the name of the applications you usually don't close after -Name
 
 $Apps = Get-Process -Name Brave, Discord , Spotify, Origin, Docker
 
+#This will go through each process and stop them 
 foreach ($Application in $Apps) {
     Stop-Process $Apps -Verbose
 }
 
+Start-Sleep 5 
+
 Write-Host "Close your most used Applications"
 
-
+# PC will shutdown after a min
 shutdown /s 
